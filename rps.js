3c8bf2b0b5a562321d99a615ed61c3defe88991f
Jonhny <jonhny.ha.nguyen@gmail.com>
2022-05-05 23:21:23 -0500

    finished the second paper rock scissors project

--- rps.js
+++ rps.js
@@ -1,17 +1,35 @@
 
-let playerScore = 0;
-let computerScore = 0;
-let draw = 0;    
-const buttons = document.querySelectorAll('button');
-const randomChoice = ["Rock", "Paper", "Scissors"]
-
-function computerPlay(randomChoice)
+  let playerScore = 0;
+  let computerScore = 0;
+  let draw = 0;
+//Function for randomizing computer's choice of Rock, Paper, Scissors
+function computerPlay()
 {
+    let randomChoice = ["Rock", "Paper", "Scissors"]
     return randomChoice[Math.floor(Math.random()*randomChoice.length)];
 }
 
+
+
+//Function for playing an entire game of Rock, Paper, Scissors
+ function game()
+ {
+        const buttons = document.querySelectorAll('button');
+        buttons.forEach((button) => {
+        button.addEventListener('click',()=>{
+        playerSelection = button.className;
+        const computerSelection = computerPlay();
+        round.textContent = (playRound(playerSelection, computerSelection));
+        pscore.textContent = "(" +playerScore + " Player) ";
+        cscore.textContent = "(" +computerScore + " Computer) ";
+        theWinner();
+        })
+})
+ }
+ //Function for playing a round of Rock, Paper, Scissors
  function playRound(playerSelection, computerSelection) 
 {
+    
     if(playerSelection.toLowerCase() === "Rock".toLowerCase() )
     {
         if( computerSelection === "Rock")
@@ -66,47 +84,66 @@ function computerPlay(randomChoice)
             return "You win!";
         }
     }
+    
 }
+ 
 
+//Creating div for displaying results
+const container = document.querySelector('#container');
+const div = document.createElement('div');
+div.classList.add('div');
+div.textContent = "Pick one";
+container.append(div);
+        const computerSelection = computerPlay();
+//Turn console.log into DOM methods
+         const results = document.querySelector('#results'); 
+        //  console.log(playRound(playerSelection, computerSelection));
+        const round = document.createElement('round');
+        round.classList.add('div');
+        results.append(round);
+        //  console.log(playerScore + " Player");
+        const pscore = document.createElement('pscore');
+        pscore.classList.add('div');
+        results.append(pscore);
+        //  console.log(computerScore + " Computer");
+        const cscore = document.createElement('cscore');
+        cscore.classList.add('div');
+        results.append(cscore);
 
+        //create the Dom for the winning and losing screen
+        const winner = document.createElement('winner');
+        winner.classList.add('p');
+        results.append(winner);
+        const loser = document.createElement('loser');
+        loser.classList.add('p');
+        results.append(loser);
 
-
- function game()
- {
-
-        buttons.forEach((button) => {
-        button.addEventListener('click',()=>{
-        playerSelection = button.className;
-    //  for(let i = 0; i < 5; i++) 
-    //  {
-         const computerSelection = computerPlay(randomChoice);
-         console.log(playRound(playerSelection, computerSelection));
-         console.log(playerScore + " Player");
-         console.log(computerScore + " Computer");
-         console.log(draw + " Draw")
-        })
-//      }
-        
-})
- }
+        // Function for calculating the and displaying the winner
 function theWinner()
 {
 
-    if (playerScore > computerScore)
+    if (playerScore == 5)
     {
-         return console.log("You are the winner!");
-    }
-    else if( playerScore < computerScore)
-    {
-         return console.log("You are the Loser!");
-    }
-    else
-    {
-         return console.log("It's a draw!");
-    }
-}
+          winner.textContent = "-You are the winner!";
+          document.getElementById('rockbtn').disabled = true;
+          document.getElementById('paperbtn').disabled = true;
+          document.getElementById('scissorsbtn').disabled = true;
 
+
+    }
+    else if ( computerScore == 5)
+    {
+          loser.textContent = "-The computer wins!";
+          document.getElementById('rockbtn').disabled = true;
+          document.getElementById('paperbtn').disabled = true;
+          document.getElementById('scissorsbtn').disabled = true;
+
+
+    }
+ 
+}
+//calling functions
 game();
-theWinner();
+// theWinner();
 
 
